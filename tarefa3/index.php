d129b23a42307ebdd67fdb52ca920562234d2ec4
Lucas Almeida <contato.lucassamuel@gmail.com>
2025-11-12 14:26:37 -0300

    Adicionando tarefa 3

---
+++ tarefa3/index.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once '../header.php';
+
+//Enunciado:
+// Uma empresa concederá um aumento de salário aos seus funcionários, variável de acordo com o cargo, conforme a tabela abaixo. 
+// Faça um algoritmo que leia o salário e o cargo de um funcionário e calcule o novo salário. 
+// Se o cargo do funcionário não estiver na tabela, ele deverá, então, receber 40% de aumento. Mostre o salário antigo, o novo salário e a diferença. 
+
+// Código	Cargo	    Percentual
+// 101	    Gerente	    10%
+// 102	    Engenheiro	20%
+// 103	    Técnico	    30%
+
+
+//Realize aqui a sua lógica:
+//
+//
+//
+
+?>
+
+<body class="bg-light">
+
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+
+                <div class="card shadow-sm border-0">
+                    <div class="card-body">
+                        <h4 class="card-title text-center mb-3">Exercício do proletariado</h4>
+                        <p class="text-secondary">
+                            Lê o enunciado por favor
+                        </p>
+
+                        <hr>
+
+                        <p><b>Código informado: </b></p>
+                        <p><b>Cargo: </b></p>
+                        <p><b>Salário informado:</b></p>
+                        <ul>
+                            <li>Percentual de aumento: $a</li>
+                        </ul>
+
+                        <div class='alert alert-success mt-3'>
+                            O salário pós aumento é: =D
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</body>
