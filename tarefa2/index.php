4f2d24f8259726e2a1dc9583cbea89809d07a694
Lucas Almeida <contato.lucassamuel@gmail.com>
2025-11-12 14:16:19 -0300

    Adicionando tarefa 2

---
+++ tarefa2/index.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once '../header.php';
+
+//Enunciado:
+// Faça um algoritmo que leia um nº inteiro e mostre uma mensagem indicando se este número é par ou ímpar
+
+//Realize aqui a sua lógica:
+//
+//
+//
+
+?>
+
+<body class="bg-light">
+
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+
+                <div class="card shadow-sm border-0">
+                    <div class="card-body">
+                        <h4 class="card-title text-center mb-3">Exercício: Ìmpar ou Par</h4>
+                        <p class="text-secondary">
+                            Faça um algoritmo que leia um nº inteiro e mostre uma mensagem indicando se este número é par ou ímpar.
+                        </p>
+
+                        <hr>
+
+                        <p><b>Número informado:</b></p>
+                        <ul>
+                            <li>Número : $a</li>
+                        </ul>
+
+                        <div class='alert alert-success mt-3'>
+                            O número é: =D
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</body>
